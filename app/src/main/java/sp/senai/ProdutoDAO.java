804a02af1ad6ab3535253d4bb1149945b0df7284
Cristiano Bennertz <cristiano.bennertz@gmail.com>
2023-07-13 16:22:27 -0300

    Ajuste no nome da tabela "produtos" para "produto". Cursor com dados de quantidade e preço passou a usar getDouble ao invés de getFloat.

--- app/src/main/java/sp/senai/ProdutoDAO.java
+++ app/src/main/java/sp/senai/ProdutoDAO.java
@@ -28,13 +28,13 @@ public class ProdutoDAO {
 
     public List<Produto> obterTodos() {
         List<Produto> produtos = new ArrayList<>();
-        Cursor cursor = banco.query("produtos", new String[]{"produtoId", "produtoNome", "produtoQuantidade", "produtoPreco"}, null, null, null, null, null);
+        Cursor cursor = banco.query("produto", new String[]{"produtoId", "produtoNome", "produtoQuantidade", "produtoPreco"}, null, null, null, null, null);
         while (cursor.moveToNext()) {
             Produto p = new Produto();
             p.setProdutoId(cursor.getInt(0));
             p.setProdutoNome(cursor.getString(1));
-            p.setProdutoQuantidade(cursor.getFloat(2));
-            p.setProdutoPreco(cursor.getFloat(3));
+            p.setProdutoQuantidade(cursor.getDouble(2));
+            p.setProdutoPreco(cursor.getDouble(3));
             produtos.add(p);
         }
         return produtos;
